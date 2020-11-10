aa52851c637847457e3ad9f6aa2ad9b7a31a598c
Andrii Vynokurov <vinokurov.andrei89@gmail.com>
2020-11-10 13:00:49 +0200

    Add png image in all size

---
+++ workshop-lesson-three/src/main/java/com/vynokurov/workshop_lesson_three/LessonThreeFirstStepActivity.kt
@@ -0,0 +1,13 @@
+package com.vynokurov.workshop_lesson_three
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class LessonThreeFirstStepActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+    }
+}
