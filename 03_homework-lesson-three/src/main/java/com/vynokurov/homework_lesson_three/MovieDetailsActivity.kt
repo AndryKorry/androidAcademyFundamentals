bca9e8bb66c28d6974e261aa3c89a223b945b4eb
Andrii Vynokurov <vinokurov.andrei89@gmail.com>
2020-11-19 16:36:57 +0200

    added open logic

--- 03_homework-lesson-three/src/main/java/com/vynokurov/homework_lesson_three/MovieDetailsActivity.kt
+++ 03_homework-lesson-three/src/main/java/com/vynokurov/homework_lesson_three/MovieDetailsActivity.kt
@@ -1,5 +1,7 @@
 package com.vynokurov.homework_lesson_three
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,4 +12,9 @@ class MovieDetailsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_movie_details)
     }
 
+    companion object {
+        @JvmStatic
+        fun newIntent(context: Context): Intent =
+            Intent(context, MovieDetailsActivity::class.java)
+    }
 }
