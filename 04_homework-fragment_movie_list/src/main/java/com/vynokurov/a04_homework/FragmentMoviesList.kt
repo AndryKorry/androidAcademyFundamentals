95e8b1491104cf15de7a36bd285f5d1cf13ee632
Andrii Vynokurov <vinokurov.andrei89@gmail.com>
2020-11-26 13:01:36 +0200

    add FragmentMoviesList.kt

---
+++ 04_homework-fragment_movie_list/src/main/java/com/vynokurov/a04_homework/FragmentMoviesList.kt
@@ -0,0 +1,17 @@
+package com.vynokurov.a04_homework
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentMoviesList : Fragment()  {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_movies_list, container, false)
+
+}
